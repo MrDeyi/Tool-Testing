77db80cdadf15d3d95c5e7c0f4b09184d2466ab4
mandilakedeyi <mikeday078@gmail.com>
2025-08-25 18:28:18 +0200

    new

--- React Js/myApp/src/App.jsx
+++ React Js/myApp/src/App.jsx
@@ -1,28 +1,51 @@
 import './App.css';
+import reactCoreConcepts from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
+
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
+  let wordIndex =  Math.floor(Math.random() * (max + 1))
+  return reactDescriptions[wordIndex];
 }
 
 function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactCoreConcepts} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are going to build!
+        {genRandomInt(reactDescriptions.length)} React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
 }
+
+function CoreConcept({image, title, description}) {
+  return ( 
+      <li>
+        <img src={image} alt={title}/>
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
+   );
+}
+
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+         <section id="core-concepts">
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+         </section>
+    
       </main>
     </div>
   );
